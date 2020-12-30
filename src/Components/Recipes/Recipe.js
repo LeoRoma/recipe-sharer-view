818072e6f4c42a52bbe4fc33f5349742e9158891
LeoRoma <xiajtn@gmail.com>
2020-12-30 16:36:17 +0000

    Date formatted DD/MM/YYYY

--- src/Components/Recipes/Recipe.js
+++ src/Components/Recipes/Recipe.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const Recipe = ({recipeId, recipeName, description, difficulty, preparationTime, cookingTime, additionalTime, servings, postDate, imageId, imageSuffix, username, userId, getRecipeInfo}) => {
+const Recipe = ({ recipeId, recipeName, description, difficulty, preparationTime, cookingTime, additionalTime, servings, postDate, imageId, imageSuffix, username, userId, getRecipeInfo }) => {
 
-var imageDomain = `https://localhost:44330/dynamic/images/${imageId}${imageSuffix}`;
-var recipePagePath = `/${recipeName}`
-    return(
+    var imageDomain = `https://localhost:44330/dynamic/images/${imageId}${imageSuffix}`;
+    var recipePagePath = `/${recipeName}`;
+   
+    var YYYY = postDate.slice(0,4);
+    var MM = postDate.slice(5, 7);
+    var DD = postDate.slice(8, 10);
+    var formattedDate = `${DD}/${MM}/${YYYY}`;
+
+    return (
         <div>
-            <img src={imageDomain} style={{width:"200px", height:"200px"}} alt="food"/>
+            <img src={imageDomain} style={{ width: "200px", height: "200px" }} alt="food" />
             <Link to={recipePagePath}><h1 onClick={getRecipeInfo.bind(this, recipeId, username, imageId, imageSuffix)}>{recipeName}</h1></Link>
             <h2>{description}</h2>
             <p>Difficulty: {difficulty}</p>
@@ -15,7 +21,7 @@ var recipePagePath = `/${recipeName}`
             <p>Cooking Time: {cookingTime}</p>
             <p>AdditionalTime: {additionalTime}</p>
             <p>Servings: {servings}</p>
-            <p>Posted Date: {postDate}</p>
+            <p>Posted Date: {formattedDate}</p>
             <p>Posted by: {username}</p>
         </div>
     );
