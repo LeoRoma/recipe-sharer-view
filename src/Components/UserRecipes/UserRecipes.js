a4198709eb10c29c3e765f85a2dc5ec9a7c9fa13
LeoRoma <xiajtn@gmail.com>
2020-12-31 14:42:55 +0000

    Added user greeting

--- src/Components/UserRecipes/UserRecipes.js
+++ src/Components/UserRecipes/UserRecipes.js
@@ -5,9 +5,10 @@ import UserRecipe from './UserRecipe';
 const UserRecipes = ({userRecipes}) => {
 
     var nRecipes = userRecipes.length;
-
+    var username = sessionStorage.getItem("username");
     return(
         <div>
+            <h1>Hello {username}!</h1>
             <h1>You have posted {nRecipes} recipes </h1>
             <Link to="/new-recipe"><button>Add New Recipe</button></Link>
             {userRecipes.map((userRecipe) => 
