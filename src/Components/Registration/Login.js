b99ddc8cb3934df7dd2758beb0cde4a799049699
LeoRoma <xiajtn@gmail.com>
2021-01-11 13:44:38 +0000

    Login style completed

--- src/Components/Registration/Login.js
+++ src/Components/Registration/Login.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import './Login.css';
+import { Redirect, Link } from 'react-router-dom';
 
 class Login extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            email: '',
-            password: '',
+            email: "",
+            password: "",
             redirect: false
         };
 
@@ -70,18 +71,34 @@ class Login extends Component {
         // console.log(sessionStorage.getItem('userId'))
         // console.log(sessionStorage.getItem('token'))
         return (
-            <div>
+            <div className="login-container">
                 {this.renderRedirect()}
                 <form className="form" onSubmit={this.handleSubmit}>
-                    <label>
-                        Email:
-                        <input type="text" name="email" onChange={this.handleChangeEmail} />
-                    </label>
-                    <label>
-                        Password:
-                        <input type="password" name="password" onChange={this.handleChangePassword} />
-                    </label>
-                    <input type="submit" value="Submit" />
+                    <div className="login-header">
+                        <h1>Sign In to Recipe Sharer</h1>
+                    </div>
+                    <div>
+                        <label>
+                            Email Address:
+                            <br />
+                            <input type="text" name="email" onChange={this.handleChangeEmail} />
+                        </label>
+                    </div>
+
+                    <div>
+                        <label>
+                            Password:
+                            <br />
+                            <input type="password" name="password" onChange={this.handleChangePassword} />
+                        </label>
+                    </div>
+
+
+                    <input className="btn-primary signin-button" type="submit" value="SIGN IN" />
+
+                    <div className="not-account">
+                        <p>Don't have an account? <Link to="/register">Sign Up</Link> </p>
+                    </div>
                 </form>
             </div>
         )
