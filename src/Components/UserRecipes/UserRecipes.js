33aa91ae388439ae4f09875dd8296f7a81f6018e
LeoRoma <xiajtn@gmail.com>
2021-01-18 15:13:23 +0000

    Display User recipes sorted by latest

--- src/Components/UserRecipes/UserRecipes.js
+++ src/Components/UserRecipes/UserRecipes.js
@@ -7,12 +7,16 @@ const UserRecipes = ({ userRecipes, getUserRecipeId }) => {
 
     var nRecipes = userRecipes.length;
     var username = sessionStorage.getItem("username");
+
+    const recipesSorted = userRecipes.sort(function (a, b) { return a.recipeId - b.recipeId});
+
+    const reversedRecipes = recipesSorted.reverse();
     return (
         <div className="user-recipes-container">
             <h1>Hello {username}!</h1>
             <h1>You have posted {nRecipes} recipes </h1>
             <Link to="/new-recipe"><button className="btn-outline-primary">Add New Recipe</button></Link>
-            {userRecipes.map((userRecipe) =>
+            {reversedRecipes.map((userRecipe) =>
                 <UserRecipe
                     key={userRecipe.recipeId}
                     recipeId={userRecipe.recipeId}
